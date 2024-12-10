50178aeb16d2fb6c5ac314ec41ecf15d622490ab
Pooja Kumbhar <pooja.p.kumbhar@gmail.com>
2024-12-10 12:03:17 +0100

    Create the counter using vue JS

---
+++ vue-counter/vue-project/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+const count=ref(0);
+</script>
+
+<template>
+<main>
+  <div>
+    <h1>The Count is:</h1>
+    <h3>{{ count }}</h3>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+  </div>
+</main>
+</template>
+
+<style scoped>
+main{
+background-color: rgb(127, 167, 154);
+display: flex;
+align-items: center;
+justify-content: center;
+height: 100vh;
+width: 100vw;
+}
+div{
+  text-align: center;
+}
+</style>
+
